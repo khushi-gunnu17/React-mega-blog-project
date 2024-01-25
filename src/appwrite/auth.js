0462477671012a7938c5e8008d07e07c79590560
khushi-gunnu17 <khushiastrogeek@gmail.com>
2024-01-25 17:55:35 +0530

    A mega blog project with react.

---
+++ src/appwrite/auth.js
@@ -0,0 +1,85 @@
+// service 
+
+import config from '../config.js';
+import { Client, Account, ID } from "appwrite";
+
+// from docs in appwrite 
+
+/*
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+    .setProject('<PROJECT_ID>');               // Your project ID
+
+const account = new Account(client);
+
+const promise = account.create('[USER_ID]', 'email@example.com', '');
+
+promise.then(function (response) {
+    console.log(response); // Success
+}, function (error) {
+    console.log(error); // Failure
+});
+*/
+
+// we have done the above code in class below 
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(client);
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+
+            if (userAccount) {
+                // call another method
+                return this.login({email, password});
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite Error :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite Error :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService()       // object
+
+export default authService
+
+
+// If any backend services changes like to a Firebase or your own backend service, then this file will only be changed.
+// same for appwrite other projects
