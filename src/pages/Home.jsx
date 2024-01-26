45fa95afcc1f8575ad110bd20acad0f6416348ef
khushi-gunnu17 <khushiastrogeek@gmail.com>
2024-01-26 22:33:24 +0530

    Project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react";
+import appwriteService from '../appwrite/config'
+import {Container, PostCard} from '../components'
+
+function Home() {
+    
+}
+
+export default Home
